4bad24fcb971e18f6931ddf33393ec8f18f845d9
imshii <75594425+imshii@users.noreply.github.com>
2021-02-16 20:32:59 -0500

    Delete bot directory

--- bot/README.md
+++
@@ -1,8 +0,0 @@
-Discord bot made by imshy
-
-needed libraries:
-    discord.py
-    matplotlib
-    requests
-    lxml
-    bs4
--- bot/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Discord Bot
-python3 bot.py
-pause
-
-REM This discordbot is made for me only, but
-REM you can use it aswell by adding a token
-REM at the bottom of "bot.py"
